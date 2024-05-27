0d0cf4583b05d5e7cf7e3c2bee58d46f06623777
Whitney Martin <158221290+whitl0w@users.noreply.github.com>
2024-05-26 20:04:10 -0400

    Add files via upload

--- code.java
+++ code.java
@@ -3,22 +3,18 @@
 import java.util.*;
 
 public class code{
-              //this is the static method which returns the nth fibonacci number
   public static int fib(int n){
     if( n<=1)
       return n;
     
-             //this method is a recursive step to add the last 2 fibonacci numbers
     return fib(n-1) + fib(n-2);
   }
   
   public static void main(String args[]){
     int n =10;
     
-            //this method calls the variable with n
     int y = fib(n);
     
-           //this command will print the results of the sequence
     System.out.println("The "+n+"th term of the Fibonacci sequence is "+y);
   }
 }
